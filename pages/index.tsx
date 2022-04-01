2b412f1a96607d66320ce9e1788eeb102a38046b
ubong <ubongnt@gmail.com>
2022-04-01 02:41:02 +0100

    home page

--- pages/index.tsx
+++ pages/index.tsx
@@ -9,7 +9,15 @@ const Home: NextPage = () => {
         <title>NFT Drop</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-        <h2 className='text-4xl text-red-500 font-bold'>Welcome to the NFT Drop Challenge</h2>
+      <div className='flex flex-1'>
+        <h2 className="text-4xl font-bold text-red-500">
+          Welcome to the NFT Drop Challenge
+        </h2>
+       <button className='h-10 bg-red-600 text-white w-full font-bold rounded-full mt-6'>Click Here</button>
+      </div>
+      <div>
+      <img src="/nft2.png" alt="" />
+      </div>
     </div>
   )
 }
